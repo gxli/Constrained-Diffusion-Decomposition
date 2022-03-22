b9e749d211730dad1db4ff506b9b5e7df6568cee
gxli <ligx.ngc7293@gmail.com>
2022-03-22 18:11:07 +0800

    added usages

---
+++ usage/use_bash.sh
@@ -0,0 +1,3 @@
+# assuming the data is contained in the input.fits
+python3  constrained_diffusion_decomposition.py input.fits 
+# the output will be written as input.fits_scale.fits
---
+++ usage/use_python.py
@@ -0,0 +1,4 @@
+# assuming that data is a N-dimensional array 
+import  constrained_diffusion_decomposition as cdd
+# the decomposition can be computed via
+result, residuals = cdd.constrained_diffusion_decomposition(data)
